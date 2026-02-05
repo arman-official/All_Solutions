19c462b74d7cfdd1f3c163eeed274db7fee19c24
Arman gupta <149881861+arman-official@users.noreply.github.com>
2026-02-05 11:40:33 +0530

    Added solution - LeetHub

---
+++ 700221-lca-in-binary-tree-GFG/700221-lca-in-binary-tree.java
@@ -0,0 +1,29 @@
+/*
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+}*/
+
+class Solution {
+    Node lca(Node root, int n1, int n2) {
+        // code here
+        if(root==null)return null;
+        if(root.data==n1 || root.data==n2)return root;
+        
+        Node left=lca(root.left,n1,n2);
+        Node right=lca(root.right,n1,n2);
+        
+        if(left!=null && right!=null)return root;
+        else if(left!=null && right==null)return left;
+        else if(right!=null && left==null)return right;
+        
+        else return null;
+        
+    }
+}

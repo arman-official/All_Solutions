0fd1ceedbd9dc94a26b12d5dbdea45c56c296e13
Arman gupta <149881861+arman-official@users.noreply.github.com>
2026-02-19 01:48:05 +0530

    Added solution - LeetHub

--- 704556-matrix-chain-multiplication-GFG/704556-matrix-chain-multiplication.java
+++ 704556-matrix-chain-multiplication-GFG/704556-matrix-chain-multiplication.java
@@ -2,30 +2,29 @@ class Solution {
     static int matrixMultiplication(int arr[]) {
         // code here
         int n=arr.length;
-        int[][] dp=new int[n+1][n+1];
-        for(int[] i:dp)
-        Arrays.fill(i,-1);
-        // for(int i=0;i<=n;i++)dp[i][i]=1;
+        int[][] dp=new int[n][n];
+        for(int[] i:dp)Arrays.fill(i,0);
+        for(int i=n-1;i>0;i--){
+            for(int j=i+1;j<n;j++){
+                int min=Integer.MAX_VALUE;
+                for(int k=i;k<j;k++){
+                    int steps= (arr[i-1]*arr[k]*arr[j])+dp[i][k]+dp[k+1][j];
+                    min=Math.min(min,steps);
+                }
+                dp[i][j]=min;
+            }
+        }
         
-        // for(int i=0;i<=n;i++){
-            
-        // }
         
-        
-        return fun(arr,1,n-1,dp);
+        return dp[1][n-1];
     }
-    static int fun(int arr[],int i,int j,int[][] dp){
-        if(dp[i][j]!=-1)return dp[i][j];
-        if(i==j){dp[i][j]=0;return 0;}
+    // static int fun(int[] arr,int i,int j){
+    //     if(i==j)return 0;
         
-        int min= Integer.MAX_VALUE;
+    //     for(int k=i;k<j;k++){
+    //         int steps= (arr[i-1]*arr[k]*arr[j])+fun(arr,i,k)+fun(arr,k+1,j);
+    //         min=Math.min(min,steps);
+    //     }return min;
         
-        for(int k=i;k<j;k++){
-            int temp= fun(arr,i,k,dp)+fun(arr,k+1,j,dp)+ arr[i-1]*arr[k]*arr[j];
-            min= Math.min(min,temp);
-            
-        }dp[i][j]=min;
-        return min;
-        
-    }
+    // }
 }

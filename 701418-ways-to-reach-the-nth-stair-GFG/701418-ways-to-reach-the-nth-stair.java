913a4cc81a7ee382786b551331360f41b73ba210
Arman gupta <149881861+arman-official@users.noreply.github.com>
2026-02-18 22:18:57 +0530

    Added solution - LeetHub

---
+++ 701418-ways-to-reach-the-nth-stair-GFG/701418-ways-to-reach-the-nth-stair.java
@@ -0,0 +1,14 @@
+class Solution {
+    static int  countWays(int n) {
+        if(n<3)return n;
+        int[] dp=new int[n+1];
+        Arrays.fill(dp,-1);
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=2;
+        for(int i=3;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }return dp[n];
+    }
+    
+}

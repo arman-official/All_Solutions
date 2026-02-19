142ef91e1bc6d9b48cc1dcdf7344babed8be7563
Arman gupta <149881861+arman-official@users.noreply.github.com>
2026-02-19 10:17:56 +0530

    Added solution - LeetHub

---
+++ 701755-steps-by-knight-GFG/701755-steps-by-knight.java
@@ -0,0 +1,43 @@
+class pair{
+    int x;
+    int y;
+    int dis;
+    pair(int x,int y,int dis){
+        this.x=x;
+        this.y=y;
+        this.dis=dis;
+    }
+}
+class Solution {
+    public int minStepToReachTarget(int knightPos[], int targetPos[], int n) {
+        // Code here
+        int[] dr={-1,-1,1,1,-2,-2,2,2};
+        int[] dc={-2,2,-2,2,-1,1,-1,1};
+        Queue<pair> q=new LinkedList<>();
+        boolean[][] vis=new boolean[n+1][n+1];
+        vis[knightPos[0]][knightPos[1]]=true;
+        q.add(new pair(knightPos[0],knightPos[1],0));
+        while(!q.isEmpty()){
+            pair temp=q.remove();
+            for(int i=0;i<8;i++){
+                int dri=temp.x+dr[i];
+                int dci=temp.y+dc[i];
+                if(dri==targetPos[0]&&dci==targetPos[1]){
+                    return temp.dis+1;
+                }
+                if(isValid(dri,dci,n)){
+                    if(!vis[dri][dci]){
+                    q.add(new pair(dri,dci,temp.dis+1));
+                    vis[dri][dci]=true;
+                    }
+                    
+                }
+            }
+        }return 0;
+        
+    }
+    public boolean isValid(int x,int y,int n){
+        if(x>=0 && x<n && y>=0 && y<n)return true;
+        return false;
+    }
+}

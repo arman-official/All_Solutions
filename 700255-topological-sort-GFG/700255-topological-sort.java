728194a1a209a3d214116e62364195bd8aa082bd
Arman gupta <149881861+arman-official@users.noreply.github.com>
2026-02-20 15:15:49 +0530

    Added solution - LeetHub

--- 700255-topological-sort-GFG/700255-topological-sort.java
+++ 700255-topological-sort-GFG/700255-topological-sort.java
@@ -1,32 +1,32 @@
 class Solution {
     public ArrayList<Integer> topoSort(int V, int[][] edges) {
         // code here
-        Stack<Integer> s=new Stack<>();
-        boolean[] vis=new boolean[V];
+        int[] degree=new int[V];
+        Queue<Integer> q=new LinkedList<Integer>();
+        ArrayList<Integer> res=new ArrayList<>();
+        
         ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
-        // int[] adj=new int[V];
-        for(int i=0;i<V;i++)
-        adj.add(new ArrayList<Integer>());
+        for(int i=0;i<V;i++)adj.add(new ArrayList<Integer>());
         for(int i=0;i<edges.length;i++){
+            degree[edges[i][1]]++;
             adj.get(edges[i][0]).add(edges[i][1]);
         }
-        for(int i=0;i<V;i++){
-            fun(i,s,vis,adj);
+        
+        for(int i=0;i<degree.length;i++){
+            if(degree[i]==0)
+            q.add(i);
         }
-        ArrayList<Integer> res=new ArrayList<>();
-        while(!s.isEmpty()){
-            res.add(s.pop());
+        
+        while(!q.isEmpty()){
+            int temp=q.remove();
+            
+            for(int i=0;i<adj.get(temp).size();i++){
+                degree[adj.get(temp).get(i)]--;
+                if(degree[adj.get(temp).get(i)]<=0)q.add(adj.get(temp).get(i));
+            }
+            res.add(temp);
         }
+        
         return res;
-    }void fun(int node,Stack<Integer> s,boolean[] vis,ArrayList<ArrayList<Integer>> adj){
-        // if(node==-1)return;
-        if(vis[node])return;
-        vis[node]=true;
-        
-        for(int i:adj.get(node)){
-            fun(i,s,vis,adj);
-        }
-        s.add(node);
-        
     }
 }

dee2aab192d0984152f6afc2be855b8ad050dc23
Arman gupta <149881861+arman-official@users.noreply.github.com>
2026-02-27 22:52:07 +0530

    Added solution - LeetHub

--- 700217-bfs-of-graph-GFG/700217-bfs-of-graph.java
+++ 700217-bfs-of-graph-GFG/700217-bfs-of-graph.java
@@ -1,28 +1,22 @@
 class Solution {
     public ArrayList<Integer> bfs(ArrayList<ArrayList<Integer>> adj) {
         // code here
-        
-        ArrayList<Integer> l=new ArrayList<>();
+        ArrayList<Integer> res=new ArrayList<>();
         Queue<Integer> q=new LinkedList<>();
-        boolean[] vis=new boolean[adj.size()];
-        fun(adj,l,q,vis);
-        return l;
-    }ArrayList<Integer> fun(ArrayList<ArrayList<Integer>> adj,ArrayList<Integer> l,Queue<Integer> q    
-    ,boolean[] vis){
-        
-        vis[0]=true;
         q.add(0);
+        boolean[] vis=new boolean[adj.size()];
+        vis[0]=true;
         while(!q.isEmpty()){
-            Integer node=q.poll();
-            l.add(node);
-            
-            for(Integer it: adj.get(node)){
-                if(!vis[it]){
-                    vis[it]=true;
-                    q.add(it);
+            int node=q.remove();
+            for(int i:adj.get(node)){
+                if(!vis[i]){
+                q.add(i);
+                vis[i]=true;
+                    
                 }
             }
+            res.add(node);
         }
-        return l;
+        return res;
     }
 }

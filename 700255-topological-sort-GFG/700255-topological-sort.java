f161e1bcb357764b779879c289e58e17a287859b
Arman gupta <149881861+arman-official@users.noreply.github.com>
2026-03-01 11:48:08 +0530

    Added solution - LeetHub

--- 700255-topological-sort-GFG/700255-topological-sort.java
+++ 700255-topological-sort-GFG/700255-topological-sort.java
@@ -1,28 +1,28 @@
 class Solution {
     public ArrayList<Integer> topoSort(int V, int[][] edges) {
-        // code here
         ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
-        for(int i=0;i<V;i++)
-        adj.add(new ArrayList<>());
-        Stack<Integer> s=new Stack<>();
-        boolean[] vis=new boolean[V];
+        int[] indegree=new int[V];
+        for(int i=0;i<V;i++)adj.add(new ArrayList<Integer>());
         for(int i=0;i<edges.length;i++){
-            adj.get(edges[i][0]).add(edges[i][1]);
+            int u=edges[i][0];
+            int v=edges[i][1];
+            indegree[v]++;
+            adj.get(u).add(v);
         }
+        Queue<Integer> q=new LinkedList<>();
         for(int i=0;i<V;i++){
-            fun(s,adj,vis,i);
+            if(indegree[i]==0)q.add(i);
         }
-        ArrayList<Integer> res=new ArrayList<>();
-        while(!s.isEmpty()){
-            res.add(s.pop());
+        ArrayList<Integer> ans=new ArrayList<>();
+        while(!q.isEmpty()){
+            int node=q.poll();
+            for(int i=0;i<adj.get(node).size();i++){
+                int nbr=adj.get(node).get(i);
+                indegree[nbr]--;
+                if(indegree[nbr]==0)q.add(nbr);
+            }
+            ans.add(node);
         }
-        return res;
-    }void fun(Stack<Integer> s,ArrayList<ArrayList<Integer>> adj, boolean[] vis,int node){
-        if(vis[node])return;
-        vis[node]=true;
-        for(int i=0;i<adj.get(node).size();i++){
-            fun(s,adj,vis,adj.get(node).get(i));
-        }
-        s.add(node);
+        return ans;
     }
 }
